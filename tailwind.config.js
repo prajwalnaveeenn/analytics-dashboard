26b03612081e4e69717f1e649be22059bba3e6c7
prajwalnaveeenn <139122004+prajwalnaveeenn@users.noreply.github.com>
2025-03-17 20:41:28 +0530

    Added files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}", // Adjust paths according to your project
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
